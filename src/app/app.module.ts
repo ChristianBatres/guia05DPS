fe611160efb562524487af8963fd5e2a6f80047c
batres <cbatresaleman97@gmail.com>
2020-09-24 23:57:22 -0600

    realizacion guia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { AlumnoListComponent } from './components/alumnos/alumno-list/alumno-list.component';
+import { AlumnoComponent } from './components/alumnos/alumno/alumno.component';
+
+// firebase
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+//import { AngularFirestoreModule } from '@angular/fire/firestore';
+//import { AngularFireStorageModule } from '@angular/fire/storage';
+//import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// service
+import { AlumnoService } from './services/alumno.service';
+
+// Toastr, para notificaciones en angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    AlumnoListComponent,
+    AlumnoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    AlumnoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
